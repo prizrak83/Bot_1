8965c4c554795d395ccd6abc8c288a9c2c764cec
prizrak83 <prizrak83@gmail.com>
2020-05-18 22:02:48 +0300

    Добавлена справка и выбор размера страницы

--- Bot_1.py
+++ Bot_1.py
@@ -4,6 +4,8 @@ import fitz
 import json
 
 
+HELP_TEXT = 'Для запроса пароля узла введите \n /list <колличество строк на странице> <имя узла> \n' \
+            'если колличество строк не задано то оно равно 5'
 f = open('token.txt')
 bot = tb.TeleBot(f.read())
 f.close()
@@ -24,44 +26,56 @@ def callback_worker(call):
     keyboard = tb.types.InlineKeyboardMarkup()
     bot.delete_message(call.message.chat.id, call.message.message_id)
     if call.data.startswith('!'):
-        if call.data[2] == ' ':
-            page = int(call.data[1])
-            mask = call.data[3:]
-        elif call.data[3] == ' ':
-            page = int(call.data[1:3])
+        page_size = int(call.data[1])
+        if call.data[3] == ' ':
+            page = int(call.data[2])
             mask = call.data[4:]
-        else:
-            page = int(call.data[1:4])
+        elif call.data[4] == ' ':
+            page = int(call.data[2:4])
             mask = call.data[5:]
+        else:
+            page = int(call.data[2:5])
+            mask = call.data[6:]
         files = glob.glob('./pass/*' + mask + '*.*')
         if page == 0:
-            for name in files[0:5]:
+            for name in files[0:page_size]:
                 keyboard.add(tb.types.InlineKeyboardButton(text=name[7:-4], callback_data=name[7:40]))
-            keyboard.add(tb.types.InlineKeyboardButton(text='>>', callback_data='!'+str(page+1)+' '+mask))
+            keyboard.add(tb.types.InlineKeyboardButton(text='>>', callback_data='!'+str(page_size)+str(page+1)+' '+mask))
         else:
-            keyboard.add(tb.types.InlineKeyboardButton(text='<<', callback_data='!'+str(page-1)+' '+mask))
-            for name in files[page*5+0:page*5+4]:
+            keyboard.add(tb.types.InlineKeyboardButton(text='<<', callback_data='!'+str(page_size)+str(page-1)+' '+mask))
+            for name in files[page*page_size+0:page*page_size+page_size]:
                 keyboard.add(tb.types.InlineKeyboardButton(text=name[7:-4], callback_data=name[7:40]))
-            if len(files)-(page*5) > 5:
-                keyboard.add(tb.types.InlineKeyboardButton(text='>>', callback_data='!'+str(page+1)+' '+mask))
+            if len(files)-(page*page_size) > page_size:
+                keyboard.add(tb.types.InlineKeyboardButton(text='>>', callback_data='!'+str(page_size)+str(page+1)+' '+mask))
         bot.send_message(call.message.chat.id, 'список', reply_markup=keyboard)
     else:
         bot.send_message(call.message.chat.id, xps(call.data))
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'help'])
 def start_message(message):
     print(message.chat.id)
 
     if not(message.chat.id in acl):
         bot.send_message(message.chat.id, 'Ошибка доступа')
         return
+    bot.send_message(message.chat.id, HELP_TEXT)
 
 
 @bot.message_handler(commands=['list'])
 def text_message(message):
     global acl
-    mask = message.text[6:]
+    if message.text[7] != ' ':
+        page_size = 5
+        mask = message.text[6:]
+    elif int(message.text[6]) < 5:
+        page_size = 5
+        mask = message.text[6:]
+    else:
+        page_size = int(message.text[6])
+        mask = message.text[8:]
+    print(mask)
+    print(page_size)
 
     if not(message.chat.id in acl):
         bot.send_message(message.chat.id, 'Ошибка доступа')
@@ -69,14 +83,14 @@ def text_message(message):
 
     keyboard = tb.types.InlineKeyboardMarkup()
     files = glob.glob('./pass/*'+mask+'*.*')
-    if len(files) < 6:
+    if len(files) < (page_size+1):
         for name in files:
             keyboard.add(tb.types.InlineKeyboardButton(text=name[7:-4], callback_data=name[7:40]))
     else:
         keyboard = tb.types.InlineKeyboardMarkup()
-        for name in files[0:5]:
+        for name in files[0:page_size]:
             keyboard.add(tb.types.InlineKeyboardButton(text=name[7:-4], callback_data=name[7:40]))
-        keyboard.add(tb.types.InlineKeyboardButton(text='>>', callback_data='!1 '+mask))
+        keyboard.add(tb.types.InlineKeyboardButton(text='>>', callback_data='!'+str(page_size)+'1 '+mask))
 
     bot.send_message(message.chat.id, 'список', reply_markup=keyboard)
 
