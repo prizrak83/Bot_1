8d158ac07e90cb32964df53175a0c7b2045341c0
prizrak83 <prizrak83@gmail.com>
2020-05-18 11:04:16 +0300

    Уменьшени списка глобальных переменных

--- Bot_1.py
+++ Bot_1.py
@@ -5,30 +5,28 @@ import fitz
 f = open('token.txt')
 bot = tb.TeleBot(f.read())
 tb.apihelper.proxy = {'https': 'socks5://14611055481:U777Vluhz8@orbtl.s5.opennetwork.cc:999'}
-page = 0
 files = []
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-    global page, files
+    global files
+    print(call.data)
+    print(call.data[4:])
     keyboard = tb.types.InlineKeyboardMarkup()
     bot.delete_message(call.message.chat.id, call.message.message_id)
-    if call.data == '>>' or call.data == '<<':
-        if call.data == '>>':
-            page += 1
-        else:
-            page -= 1
+    if call.data.startswith('page'):
+        page = int(call.data[4:])
         if page == 0:
             for name in files[0:5]:
                 keyboard.add(tb.types.InlineKeyboardButton(text=name[0:-4], callback_data=name[0:-4]))
-            keyboard.add(tb.types.InlineKeyboardButton(text='>>', callback_data='>>'))
+            keyboard.add(tb.types.InlineKeyboardButton(text='>>', callback_data='page'+str(page+1)))
         else:
-            keyboard.add(tb.types.InlineKeyboardButton(text='<<', callback_data='<<'))
+            keyboard.add(tb.types.InlineKeyboardButton(text='<<', callback_data='page'+str(page-1)))
             for name in files[page*5+0:page*5+4]:
                 keyboard.add(tb.types.InlineKeyboardButton(text=name[0:-4], callback_data=name[0:-4]))
             if len(files)-page+5 < 5:
-                keyboard.add(tb.types.InlineKeyboardButton(text='>>', callback_data='>>'))
+                keyboard.add(tb.types.InlineKeyboardButton(text='>>', callback_data='page'+str(page+1)))
         bot.send_message(call.message.chat.id, 'список', reply_markup=keyboard)
     else:
         bot.send_message(call.message.chat.id, xps(call.data))
@@ -46,7 +44,7 @@ def start_message(message):
         keyboard = tb.types.InlineKeyboardMarkup()
         for name in files[0:5]:
             keyboard.add(tb.types.InlineKeyboardButton(text=name[0:-4], callback_data=name[0:-4]))
-        keyboard.add(tb.types.InlineKeyboardButton(text='>>', callback_data='>>'))
+        keyboard.add(tb.types.InlineKeyboardButton(text='>>', callback_data='page1'))
 
     bot.send_message(message.chat.id, 'список', reply_markup=keyboard)
 
